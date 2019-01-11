56cfe51ba7824e66b2745b504d102619a9f27118
Chris Midgley <chris.midgley@bjss.com>
2019-01-11 10:54:32 +0000

    $(My.Variable) notation works in inline scripts but not files.

--- deploy/deploy_to_production.sh
+++ deploy/deploy_to_production.sh
@@ -1,1 +1,1 @@
-az webapp deployment source config-zip --resource-group nhsuk-service-manual --name nhsuk-service-manual --src $(System.DefaultWorkingDirectory)/_nhsuk.nhsuk-service-manual/drop/*.zip
+az webapp deployment source config-zip --resource-group nhsuk-service-manual --name nhsuk-service-manual --src $SYSTEM_DEFAULTWORKINGDIRECTORY/_nhsuk.nhsuk-service-manual/drop/*.zip
--- deploy/deploy_to_staging.sh
+++ deploy/deploy_to_staging.sh
@@ -1,1 +1,1 @@
-az webapp deployment source config-zip --resource-group nhsuk-service-manual --name nhsuk-service-manual-staging --src $(System.DefaultWorkingDirectory)/_nhsuk.nhsuk-service-manual/drop/*.zip
+az webapp deployment source config-zip --resource-group nhsuk-service-manual --name nhsuk-service-manual-staging --src $SYSTEM_DEFAULTWORKINGDIRECTORY/_nhsuk.nhsuk-service-manual/drop/*.zip
