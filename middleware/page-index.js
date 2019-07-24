9f99703870a4e4a33b4f656bf953b7b717fb688f
xLasercut <tianxun.wang2004@gmail.com>
2019-07-24 10:46:08 +0100

    fixed word casing in search. fixed search link opening new window

--- middleware/page-index.js
+++ middleware/page-index.js
@@ -89,9 +89,9 @@ class PageIndex {
 
   _searchIndex(query) {
     return this.index.query(function(q) {
-      q.term(query, { usePipeline: true, boost: 100 })
+      q.term(query.toLowerCase(), { usePipeline: true, boost: 100 })
       // look for terms that match the beginning of this query and apply a medium boost
-      q.term(query + "*", { usePipeline: false, boost: 10 })
+      q.term(query.toLowerCase() + "*", { usePipeline: false, boost: 10 })
     })
   }
 
