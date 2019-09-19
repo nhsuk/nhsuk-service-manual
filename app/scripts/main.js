8f4303ff16760a937a313f630bfd23a9587d4b57
AdamChrimes <iChrimes@live.co.uk>
2019-09-19 13:19:40 +0100

    Revert "remove the old basic cookie consent solution"
    This reverts commit 9d0da288acc44725591312204a4bb617a622c944.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,9 +5,11 @@ import './polyfills';
 import nhsuk_skipLink from '../../node_modules/nhsuk-frontend/packages/components/skip-link/skip-link'; /* eslint-disable-line camelcase */
 // import './search';
 import DesignExample from './design-example';
+import cookieConsent from './cookieConsent';
 
 // Initialise components
 nhsuk_skipLink();
+cookieConsent();
 
 document.querySelectorAll(DesignExample.selector()).forEach((el) => {
   new DesignExample(el);
