d2029c247dc0de9ef41ec2df926d2fd0828acc32
Sabeeh UL-Hassan <sabeeh.ul-hassan1@nhs.net>
2019-12-05 11:43:42 +0000

    add result truncation

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -3,10 +3,14 @@
  * @param {string} result String containing individual result from autocomplete source function
  * @returns {string} String of HTML containing passed result
 */
-export const suggestion = obj => `
-  <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
-  ${obj.title}
-`;
+export const suggestion = (obj) => {
+  const truncateLength = 36;
+  const dots = obj.title.length > truncateLength ? '...' : '';
+  const resultTruncated = obj.title.substring(0, truncateLength) + dots;
+  return ` <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
+    ${resultTruncated}
+  `;
+};
 
 export const inputValue = (obj) => {
   if (obj && obj.title) return obj.title.trim();
