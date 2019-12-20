14d2f9e3e2ff42755a96124a8a28c95c2751af97
AdamChrimes <iChrimes@live.co.uk>
2019-12-20 10:22:56 +0000

    remove /service-manual prefix and fix linting in routing file

--- middleware/routing.js
+++ middleware/routing.js
@@ -10,37 +10,37 @@
 * 3. if error other than template not found - call next with the error
 * 4. Maybe it's a folder - try to render [path]/index.html
 * 5. We got template not found both times - call next to trigger the 404 page
-* 6. Remove the first slash and the service manual site root, render won't work with it
+* 6. Remove the first slash, render won't work with it
 * 7. If it's blank, render the root index
-**/
+* */
 
-function renderPath (path, res, next) {
-  res.render(path, function (error, html) { // [1] //
+function renderPath(path, res, next) {
+  res.render(path, (error, html) => { // [1] //
     if (!error) {
-      res.set({ 'Content-type': 'text/html; charset=utf-8' }) // [2] //
-      res.end(html)
-      return
+      res.set({ 'Content-type': 'text/html; charset=utf-8' }); // [2] //
+      res.end(html);
+      return;
     }
     if (!error.message.startsWith('template not found')) { // [3] //
-      next(error)
-      return
+      next(error);
+      return;
     }
     if (!path.endsWith('/index')) {
-      renderPath(path + '/index', res, next) // [4] //
-      return
+      renderPath(`${path}/index`, res, next); // [4] //
+      return;
     }
-    next() // [5] //
-  })
+    next(); // [5] //
+  });
 }
 
 exports.matchRoutes = function (req, res, next) {
-  var path = req.path
+  let path = req.path;
 
-  path = path.substr(16) // [6] //
+  path = path.substr(1); // [6] //
 
   if (path === '') { // [7] //
-    path = 'index'
+    path = 'index';
   }
 
-  renderPath(path, res, next)
-}
+  renderPath(path, res, next);
+};
