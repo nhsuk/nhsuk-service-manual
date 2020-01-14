ef97ae9a6c723b1423c0db3cd97b167eb57bf70a
AdamChrimes <iChrimes@live.co.uk>
2020-01-14 15:47:48 +0000

    remove unused configuration variable

--- app/config.js
+++ app/config.js
@@ -1,14 +1,11 @@
 const packageJson = require('../package.json');
 
 module.exports = {
-  // Service name
-  serviceName: 'NHS digital service manual',
+  // Base URL
+  baseURL: process.env.BASE_URL || 'https://service-manual.nhs.uk',
 
   // Environment
   env: process.env.NODE_ENV || 'development',
-
-  // Base URL
-  baseURL: process.env.BASE_URL || 'https://service-manual.nhs.uk',
 
   // Port to run local development server on
   port: process.env.PORT || 3000,
