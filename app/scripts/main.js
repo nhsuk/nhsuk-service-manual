5bdbcc15d1e033485c9815bae5a21fc7d10dc6f6
AdamChrimes <iChrimes@live.co.uk>
2020-04-24 13:53:19 +0100

    tidy up the JavaScript file with imports

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-new */
-import './polyfills';
-
-// NHS.UK frontend
+// NHS.UK frontend components
 import AutoComplete from 'nhsuk-frontend/packages/components/header/autoCompleteConfig';
 import Header from '../../node_modules/nhsuk-frontend/packages/components/header/header';
 import SkipLink from '../../node_modules/nhsuk-frontend/packages/components/skip-link/skip-link';
@@ -9,9 +7,10 @@ import Details from '../../node_modules/nhsuk-frontend/packages/components/detai
 import Checkboxes from '../../node_modules/nhsuk-frontend/packages/components/checkboxes/checkboxes';
 import Radios from '../../node_modules/nhsuk-frontend/packages/components/radios/radios';
 
-// Polyfills
+// NHS.UK frontend polyfills
 import '../../node_modules/nhsuk-frontend/packages/polyfills';
 
+// NHS digital service manual components
 import DesignExample from './design-example';
 import {
   inputValue,
@@ -20,13 +19,19 @@ import {
   suggestion,
 } from './search';
 
-// Initialise components
+// NHS digital service manual polyfills
+import './polyfills';
+
+// Initialise NHS.UK frontend components
 Header();
 Details();
 SkipLink();
 Checkboxes();
 Radios();
 
+// Initialise NHS digital service manual components
+
+// Design examples
 document.querySelectorAll(DesignExample.selector()).forEach((el) => {
   new DesignExample(el);
 });
