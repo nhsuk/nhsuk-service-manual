ee657fb48e96a25b4095ebf586e18321dcd7f04f
Dominic Baker <dominic.baker1@nhs.net>
2021-04-09 10:17:23 +0100

    change to the data-layer structure for adobe analytics

--- app/digital-data.js
+++ app/digital-data.js
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys */
 function getPageName(path) {
   return `nhs:manual${path.replace(/\/$/, '').replace(/\//g, ':')}`;
 }
@@ -12,15 +13,15 @@ function digitalData(req) {
 
   return {
     page: {
+      pageInfo: {
+        pageName: getPageName(path),
+      },
       category: {
         primaryCategory: categories[0],
         subCategory1: categories[1],
         subCategory2: categories[2],
         subCategory3: categories[3],
         subCategory4: categories[4],
-      },
-      pageInfo: {
-        pageName: getPageName(path),
       },
     },
   };
