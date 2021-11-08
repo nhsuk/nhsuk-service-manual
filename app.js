4c64739f8c4dd97b4853e6a65134dc2cb1ec58e1
Henry Cookson <henry.cookson@rocketmail.com>
2021-11-08 16:45:38 +0000

    remove redirects to the review-date and care-cards pages. Added two-colon care card example

--- app.js
+++ app.js
@@ -174,16 +174,6 @@ app.get('/content/pdfs', (req, res) => {
   res.redirect('/content/pdfs-and-other-non-html-documents');
 });
 
-// Review date redirect
-app.get('/design-system/components/review-date', (req, res) => {
-  res.redirect('/design-system/patterns/reassure-users-that-a-page-is-up-to-date');
-});
-
-// Care card redirect
-app.get('/design-system/components/care-cards', (req, res) => {
-  res.redirect('/design-system/patterns/help-users-get-the-care-they-need');
-});
-
 // Automatically route pages
 app.get(/^([^.]+)$/, (req, res, next) => {
   routing.matchRoutes(req, res, next);
