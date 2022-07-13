3b1207859180ba4e0f04d58e3c5777b1856477d3
w@rren <w@rren.dev>
2022-07-13 14:15:27 +0100

    Set production mode as default
    We can't be sure what envs are set on the staging and production boxes

--- app/config.js
+++ app/config.js
@@ -1,13 +1,14 @@
 module.exports = {
-
   // Adobe analytics
-  adobeTrackingUrl: process.env.ADOBE_TRACKING_URL || '//assets.adobedtm.com/f8560165ec6a/5d91bd521a81/launch-c545cb3a904a-development.min.js',
+  adobeTrackingUrl:
+    process.env.ADOBE_TRACKING_URL ||
+    "//assets.adobedtm.com/f8560165ec6a/5d91bd521a81/launch-c545cb3a904a-development.min.js",
 
   // Base URL
-  baseURL: process.env.BASE_URL || 'https://service-manual.nhs.uk',
+  baseURL: process.env.BASE_URL || "https://service-manual.nhs.uk",
 
   // Environment
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "production",
 
   // Port to run local development server on
   port: process.env.PORT || 3000,
