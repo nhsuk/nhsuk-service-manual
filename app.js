1dcd21e5c234adacde8bda2dda9b9e9409bafea6
Colin Rotherham <work@colinr.com>
2025-05-23 14:09:20 +0100

    Move design example query strings into front matter

--- app.js
+++ app.js
@@ -68,13 +68,7 @@ env.addFilter('markdown', filters.markdown);
 
 // Render standalone design examples
 app.get('/design-example/:group/:item/:type', (req, res) => {
-  const displayFullPage = req.query.fullpage === 'true';
-  const blankPage = req.query.blankpage === 'true';
-  const backgroundColours = ['blue', 'grey-4'];
-  const background = backgroundColours.includes(req.query.background) ? req.query.background : null;
-  const { group } = req.params;
-  const { item } = req.params;
-  const { type } = req.params;
+  const { group, item, type } = req.params;
 
   // Get the given example as HTML.
   const exampleHtml = fileHelper.getHTMLCode({
@@ -86,17 +80,10 @@ app.get('/design-example/:group/:item/:type', (req, res) => {
   });
 
   // Wrap the example HTML in a basic html base template.
-  let baseTemplate = 'layouts/design-example-wrapper.njk';
-  if (displayFullPage) {
-    baseTemplate = 'layouts/design-example-wrapper-full.njk';
-  }
-  if (blankPage) {
-    baseTemplate = 'layouts/design-example-wrapper-blank.njk';
-  }
+  const { previewLayout = 'design-example-wrapper' } = data;
 
-  res.render(baseTemplate, {
+  res.render(`layouts/${previewLayout}`, {
     ...data,
-    background,
     exampleHtml,
     item,
   });
