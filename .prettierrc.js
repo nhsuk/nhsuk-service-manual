57bd0916da0b973bd313a16e211df534cfbfd10e
Colin Rotherham <work@colinr.com>
2025-05-29 18:04:40 +0100

    Enable Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        embeddedLanguageFormatting: 'off',
+        singleQuote: false
+      }
+    },
+    {
+      files: '*.scss',
+      options: {
+        printWidth: 120,
+        singleQuote: false
+      }
+    }
+  ]
+}
