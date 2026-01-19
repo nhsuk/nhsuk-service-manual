1afa52639ad24be909c255beaed91fcc76750848
Colin Rotherham <work@colinr.com>
2026-01-19 14:01:59 +0000

    Port Nunjucks macro options helpers from GOV.UK Frontend

--- app/javascripts/design-example.mjs
+++ app/javascripts/design-example.mjs
@@ -13,7 +13,7 @@ export class DesignExample extends Component {
     this.currentTabClass = `${this.tabClass}--current`
     this.hiddenClass = 'js-hidden'
 
-    this.tabs = this.$root.querySelectorAll(`.${this.tabClass}`)
+    this.tabs = this.$root.querySelectorAll(`.${this.tabClass} a`)
     this.examples = this.$root.querySelectorAll(
       '.app-code-snippet__preformatted'
     )
@@ -22,15 +22,64 @@ export class DesignExample extends Component {
     this.iframe = this.$root.querySelector('iframe')
 
     this.bindEvents()
+
+    const hash = window.location.hash
+
+    if (hash.match('^#options-')) {
+      const $optionsElement = this.$root.querySelector(hash)
+      if (!$optionsElement) {
+        return
+      }
+
+      // Is hash for a specific options table? eg. #options-checkboxes-example--hint
+      const isLinkToTable = hash.indexOf('--') > -1
+
+      const exampleName = isLinkToTable
+        ? hash.split('#options-')[1].split('--')[0]
+        : hash.split('#options-')[1]
+
+      if (exampleName) {
+        const $tabLink = this.$root.querySelector(
+          `a[href="#${exampleName}-nunjucks"]`
+        )
+
+        const $optionsDetailsElement = this.$root.querySelector(
+          `#options-${exampleName}-details`
+        )
+
+        if (
+          !($tabLink instanceof HTMLAnchorElement) ||
+          !($optionsDetailsElement instanceof HTMLDetailsElement)
+        ) {
+          return
+        }
+
+        this.handleTabClick($tabLink)
+
+        $optionsDetailsElement.open = true
+
+        window.setTimeout(() => {
+          $tabLink.focus()
+          if (isLinkToTable) $optionsElement.scrollIntoView()
+        }, 0)
+      }
+    }
   }
 
   bindEvents() {
     this.tabs.forEach((tab) =>
-      tab.addEventListener('click', (e) => this.handleTabClick(e))
+      tab.addEventListener('click', (event) => {
+        this.handleTabClick(event.currentTarget)
+        event.preventDefault()
+      })
     )
 
     this.closeButtons.forEach((closeButton) => {
-      closeButton.addEventListener('click', (e) => this.handleCloseClick(e))
+      closeButton.addEventListener('click', (event) => {
+        this.handleCloseClick()
+        event.preventDefault()
+      })
+
       closeButton.removeAttribute('hidden')
     })
 
@@ -52,19 +101,19 @@ export class DesignExample extends Component {
     }
   }
 
-  handleTabClick(e) {
-    const targetEl = e.target.parentElement
-    const index = targetEl.dataset.index
-
-    e.preventDefault()
+  handleTabClick($tabLink) {
+    const $tabParent = $tabLink.parentElement
+    const index = $tabParent.dataset.index
 
     this.tabs.forEach((tab) => {
-      if (tab.classList.contains(this.currentTabClass)) {
-        tab.classList.remove(this.currentTabClass)
+      if (tab.href !== $tabLink.href) {
+        tab.setAttribute('aria-expanded', 'false')
+        tab.parentElement.classList.remove(this.currentTabClass)
       }
     })
 
-    targetEl.classList.add(this.currentTabClass)
+    $tabLink.setAttribute('aria-expanded', 'true')
+    $tabParent.classList.add(this.currentTabClass)
 
     this.exampleToggler(index)
   }
@@ -75,8 +124,9 @@ export class DesignExample extends Component {
     })
 
     this.tabs.forEach((tab) => {
-      if (tab.classList.contains(this.currentTabClass)) {
-        tab.classList.remove(this.currentTabClass)
+      if (tab.parentElement.classList.contains(this.currentTabClass)) {
+        tab.setAttribute('aria-expanded', 'false')
+        tab.parentElement.classList.remove(this.currentTabClass)
       }
     })
   }
