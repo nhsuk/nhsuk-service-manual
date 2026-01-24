49535bbb8b4f8d6035b7747ef531ca71483637a3
krishgera <krishgera@gmail.com>
2026-01-24 15:55:18 +0000

    Fix design example tabs init and ensure assets build correctly

--- app/javascripts/main.mjs
+++ app/javascripts/main.mjs
@@ -1,26 +1,28 @@
-// NHS.UK frontend components
+// NHS digital service manual components
 import { createAll, initAll } from 'nhsuk-frontend'
 
-// NHS digital service manual components
 import initAccessibleAutocomplete from './accessible-autocomplete.mjs'
 import { DesignExample } from './design-example.mjs'
 import { inputValue, onConfirm, source, suggestion } from './search.mjs'
 
-// Initialise NHS.UK frontend components
 initAll()
 
-// Initialise NHS digital service manual components
-initAccessibleAutocomplete({
-  form: document.querySelector('.nhsuk-header__search-form'),
-  input: document.querySelector('.nhsuk-header__search-form .nhsuk-input'),
-  button: document.querySelector('.nhsuk-header__search-form .nhsuk-button'),
-  onConfirm,
-  source,
-  templates: {
-    inputValue,
-    suggestion
-  }
-})
+const form = document.querySelector('.nhsuk-header__search-form')
+const input = document.querySelector('.nhsuk-header__search-form .nhsuk-input')
+const button = document.querySelector(
+  '.nhsuk-header__search-form .nhsuk-button'
+)
+
+if (form && input && button) {
+  initAccessibleAutocomplete({
+    form,
+    input,
+    button,
+    onConfirm,
+    source,
+    templates: { inputValue, suggestion }
+  })
+}
 
 // Design examples
 createAll(DesignExample)
