e5e30ca22ab2b76c3dbfda4049e25d835efc7a46
Colin Rotherham <work@colinr.com>
2026-02-17 00:01:09 +0000

    Set up Jest DOM with environment

--- app/javascripts/main.jsdom.test.mjs
+++ app/javascripts/main.jsdom.test.mjs
@@ -3,11 +3,6 @@ import { initAll } from 'nhsuk-frontend'
 jest.mock('nhsuk-frontend')
 
 describe('NHS digital service manual', () => {
-  beforeEach(() => {
-    document.documentElement.innerHTML = ''
-    document.body.classList.add('nhsuk-frontend-supported')
-  })
-
   describe('NHS.UK frontend', () => {
     it('should init all components', async () => {
       await import('./main.mjs')
