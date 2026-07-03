857ef18ca7264879ff80911e0a6a927abbebe96e
Colin Rotherham <work@colinr.com>
2026-07-03 14:46:11 +0100

    Allow JavaScript toggle to be set server-side

--- app/index.js
+++ app/index.js
@@ -160,9 +160,8 @@ env.addFilter('unindent', filters.unindent)
 
 // Render standalone design examples
 app.get('/design-example/:group/:item/:type', (req, res, next) => {
-  const { group, item, type } = req.params
-
-  const javascript = req.query.javascript !== 'off'
+  const { params, query } = req
+  const { group, item, type } = params
 
   // Continue to 404 page
   if (!fileHelper.hasNunjucksPath({ group, item, type })) {
@@ -187,6 +186,10 @@ app.get('/design-example/:group/:item/:type', (req, res, next) => {
 
   // Wrap the example HTML in a basic html base template.
   const { previewLayout = 'design-example-wrapper' } = data
+
+  const javascript = query.javascript
+    ? query.javascript === 'on'
+    : data.javascript !== false
 
   res.render(`layouts/${previewLayout}`, {
     ...data,
